88e1ccd64aee43a577c776f6fbd038f29f83115b
Soumil <soumil009@outlook.com>
2024-04-18 16:55:33 +0100

    basic concept 2

--- main.py
+++ main.py
@@ -1,4 +1,31 @@
-import requests
+from playwright.sync_api import sync_playwright, ViewportSize
+from time import sleep
 
-results: list[dict] = requests.get("https://www.reddit.com/r/AmItheAsshole.json").json()['data']['children']
-print(results[5]['data']['selftext_html'])
+dsf = (500 // 600) + 1
+
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless=False)
+    context = browser.new_context(
+        locale="en-us",
+        color_scheme="dark",
+        viewport=ViewportSize(width=500, height=250),
+        device_scale_factor=dsf,
+    )
+
+    # context.add_cookies(cookies)  # load preference cookies
+    page = browser.new_page()
+    page.goto("https://www.reddit.com/r/AITAFiltered/new")
+    page.wait_for_load_state()
+    sleep(2)
+    page.screenshot(path="image.png")
+    # Select the element
+    element = page.query_selector("#main-content > div:nth-child(3)")
+    # loop over the article elements in element
+    for article in element.query_selector_all("article"):
+        # get the article title
+        title = article.query_selector("shreddit-post:nth-child(3)")
+        # get the article link
+        link = article.query_selector("a").get_attribute("href")
+        print(f"Title: {title}\nLink: {link}\n")
+
+    browser.close()
